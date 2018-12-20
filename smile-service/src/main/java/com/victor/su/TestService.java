1d1873b44602a58489444971c3558ab9a4f55d48
suzhengxiao <suzhengxiao@jd.com>
2018-12-20 17:27:44 +0800

    add service

---
+++ smile-service/src/main/java/com/victor/su/TestService.java
@@ -0,0 +1,7 @@
+package com.victor.su;
+
+/**
+ * Created by suzhengxiao on 2018/12/20.
+ */
+public interface TestService {
+}
---
+++ smile-service/src/main/java/com/victor/su/impl/TestServiceImpl.java
@@ -0,0 +1,11 @@
+package com.victor.su.impl;
+
+import com.victor.su.TestService;
+
+/**
+ * Created by suzhengxiao on 2018/12/20.
+ */
+public class TestServiceImpl implements TestService {
+
+
+}
